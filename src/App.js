855bd33048760f0d853e3831b370d53a1489d638
ilir pllana <ilirpllana95@gmail.com>
2022-01-28 16:57:10 -0500

    added levels

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const App = () => {
 
-  var options = {
-    method: 'GET',
-    url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
-    params: {level: '3', area: 'sat'},
-    headers: {
-      'x-rapidapi-host': 'twinword-word-association-quiz.p.rapidapi.com',
-      'x-rapidapi-key': '6df3b81666mshecd82d66f32708cp12ddd2jsnf7bdb36865ed'
-    }
-  };
+  const [chosenLevel, setChosenLevel] = useState(1)
+  const [word1, setWord1] = useState("")
+  const [word2, setWord2] = useState("")
+  const [word3, setWord3] = useState("")
 
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  const getRandomWords = () => {
+    const options = {
+      method: 'GET',
+      url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+      params: {level: chosenLevel, area: 'sat'},
+      headers: {
+        'x-rapidapi-host': 'twinword-word-association-quiz.p.rapidapi.com',
+        'x-rapidapi-key': '6df3b81666mshecd82d66f32708cp12ddd2jsnf7bdb36865ed'
+      }
+    };
+
+    axios.request(options).then((response) =>{
+      console.log(response.data.quizlist[0].quiz[0]);
+      setWord1(response.data.quizlist[0].quiz[0])
+      setWord2(response.data.quizlist[0].quiz[1])
+      setWord3(response.data.quizlist[0].quiz[2])
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  useEffect(() => {
+    if(chosenLevel) getRandomWords()
+  }, [chosenLevel])
 
   return (
     <div className="App">
-      Hello
+      <div className="level-selector">
+        <select name="levels" id="levels" value={chosenLevel} onChange={(e) => setChosenLevel(e.target.value)}>
+          <option value={'1'} >Level 1</option>
+          <option value={'2'} >Level 2</option>
+          <option value={'3'} >Level 3</option>
+        </select>
+        <div className="quiz-words">
+          <input value={word1} disabled="true"/>
+          <input value={word2} disabled="true"/>
+          <input value={word3} disabled="true"/>
+          
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
